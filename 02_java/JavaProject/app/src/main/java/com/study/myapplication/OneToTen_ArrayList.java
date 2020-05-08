5aeb0ac2bc642edfdf8c818ded3082f18da99ebc
newdecide <liberty-1@nate.com>
2020-05-08 18:14:51 +0900

    [생성] 안드로이드 기초_0to10_배열리스트

---
+++ 02_java/JavaProject/app/src/main/java/com/study/myapplication/OneToTen_ArrayList.java
@@ -0,0 +1,14 @@
+package com.study.myapplication;
+
+import java.util.ArrayList;
+
+public class OneToTen_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> oneTotenarray = new ArrayList<>();
+        for(int i = 1; i < 11; i++){
+            oneTotenarray.add(i);
+        }
+
+        System.out.println(oneTotenarray);
+    }
+}

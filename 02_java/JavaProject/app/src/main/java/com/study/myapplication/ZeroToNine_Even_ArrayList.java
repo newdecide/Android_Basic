68ebacb6bc9c4ce2e18492588a050fba89147b75
newdecide <liberty-1@nate.com>
2020-05-10 20:52:26 +0900

    [생성] 안드로이드 기초_0to9 짝수 찾기 배열리스트

---
+++ 02_java/JavaProject/app/src/main/java/com/study/myapplication/ZeroToNine_Even_ArrayList.java
@@ -0,0 +1,27 @@
+package com.study.myapplication;
+
+import java.util.ArrayList;
+
+public class ZeroToNine_Even_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            integerArrayList.add(i);
+        }
+
+        howManyEven(integerArrayList);
+    }
+
+    public static int howManyEven(ArrayList<Integer> arrayList){
+        int result = 0;
+        System.out.print("짝수 값: ");
+        for(int i = 0; i < arrayList.size(); i++){
+            if(arrayList.get(i) % 2 == 0) {
+                result++;
+                System.out.print(arrayList.get(i)+" ");
+            }
+        }
+        System.out.println();
+        return result;
+    }
+}

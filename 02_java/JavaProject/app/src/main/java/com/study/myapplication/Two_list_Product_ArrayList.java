ae3256ff0a954232b8dd8daab036e7514514e519
newdecide <liberty-1@nate.com>
2020-05-13 22:02:40 +0900

    [생성] 안드로이드 기초_두개의 리스트 합치기_배열리스트

---
+++ 02_java/JavaProject/app/src/main/java/com/study/myapplication/Two_list_Product_ArrayList.java
@@ -0,0 +1,30 @@
+package com.study.myapplication;
+
+import java.util.ArrayList;
+
+public class Two_list_Product_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> oneTotenarray = new ArrayList<>();
+        for(int i = 1; i < 11; i++){
+            oneTotenarray.add(i);
+        }
+
+        ArrayList<Integer> tenToonearray = new ArrayList<>();
+        for(int i = 1; i < 11; i++){
+            tenToonearray.add(11 - i);
+        }
+
+        System.out.println("oneTotenarray: " + oneTotenarray);
+        System.out.println("tenToonearray: " + tenToonearray);
+        ArrayListProduct(oneTotenarray, tenToonearray);
+    }
+
+    public static void ArrayListProduct(ArrayList<Integer> arrayList1, ArrayList<Integer> arrayList2) {
+        ArrayList<Integer> productarray = new ArrayList<>();
+        for (int i = 0; i < arrayList1.size(); i++) {
+            int result = arrayList1.get(i) * arrayList2.get(i);
+            productarray.add(result);
+        }
+        System.out.println("resultarray: "+ productarray);
+    }
+}

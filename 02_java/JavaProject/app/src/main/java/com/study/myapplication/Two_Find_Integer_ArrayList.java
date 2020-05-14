203451fa9b3426bb624f1229d51912dad4a0cdf2
newdecide <liberty-1@nate.com>
2020-05-14 21:57:36 +0900

    [생성] 안드로이드 기초_숫자 2 찾기_정수클래스_배열리스트

---
+++ 02_java/JavaProject/app/src/main/java/com/study/myapplication/Two_Find_Integer_ArrayList.java
@@ -0,0 +1,24 @@
+package com.study.myapplication;
+
+import java.util.ArrayList;
+
+public class Two_Find_Integer_ArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+
+        intArrayList.add(2);
+        intArrayList.add(3);
+
+        System.out.println(intArrayList);
+
+        for (int i = 0; i < intArrayList.size(); i++){
+            if (intArrayList.get(i) == 2){
+                System.out.println("2를 찾았다");
+            }
+        }
+
+
+
+    }
+}

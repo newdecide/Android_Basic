fe9a66b2d5892ecb7ac1d6a016b2611a6be8c3a6
newdecide <liberty-1@nate.com>
2020-05-15 19:06:01 +0900

    [생성] 안드로이드 기초_타입 없는_배열리스트

---
+++ 02_java/JavaProject/app/src/main/java/com/study/myapplication/Formless_ArrayList.java
@@ -0,0 +1,17 @@
+package com.study.myapplication;
+
+import java.util.ArrayList;
+
+public class Formless_ArrayList {
+    public static void main(String[] args) {
+        ArrayList FormlessArrayList = new ArrayList();
+        FormlessArrayList.add(19);
+        FormlessArrayList.add(20);
+        FormlessArrayList.add(true);
+        FormlessArrayList.add("리스트");
+        System.out.println(FormlessArrayList);
+
+        FormlessArrayList.remove(3);
+        System.out.println(FormlessArrayList);
+    }
+}

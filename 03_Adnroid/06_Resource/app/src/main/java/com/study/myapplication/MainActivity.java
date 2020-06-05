2109d6627e6a79e5a1e8f8ae8beeca08f7e38107
newdecide <liberty-1@nate.com>
2020-06-05 21:16:18 +0900

    [추가] 안드로이드 기초_리소스 문자열 데이터 가져오기 및 이미지 세로크기 조정

--- 03_Adnroid/06_Resource/app/src/main/java/com/study/myapplication/MainActivity.java
+++ 03_Adnroid/06_Resource/app/src/main/java/com/study/myapplication/MainActivity.java
@@ -3,14 +3,13 @@ package com.study.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView dog1, dog2, dog3;
-    TextView textView;
+    ImageView dog1, dog2;
+    TextView resourceTextview, changeTextview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +18,16 @@ public class MainActivity extends AppCompatActivity {
         // xml의 뷰 컴포넌트를 가져오는 방법
         dog1 = findViewById(R.id.dog1);
         dog2 = findViewById(R.id.dog2);
-        textView = findViewById(R.id.text);
+        resourceTextview = findViewById(R.id.text);
+        changeTextview = findViewById(R.id.chagetext);
 
         // xml의 뷰 컴포넌트를 동적으로 변경하는 방법
         dog2.setImageResource(R.drawable.dog3);
-        textView.setText("안녕하십니까");
+        resourceTextview.setText("안녕하십니까");
 
+        // res에 정의된 string을 가져오는 방법
+        String content = getApplicationContext().getResources().getString(R.string.define_string);
+        changeTextview.setText(content);
+        
     }
 }

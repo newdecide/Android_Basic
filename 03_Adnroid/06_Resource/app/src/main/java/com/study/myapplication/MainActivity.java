3b2de07bade886a083d3789dd7e5af5c479d3c62
newdecide <liberty-1@nate.com>
2020-06-07 20:38:09 +0900

    [추가] 안드로이드 기초_여러나라 인사 문자열 담은 것 화면에 보여주기

--- 03_Adnroid/06_Resource/app/src/main/java/com/study/myapplication/MainActivity.java
+++ 03_Adnroid/06_Resource/app/src/main/java/com/study/myapplication/MainActivity.java
@@ -8,10 +8,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView dog1, dog2;
     TextView resourceTextview, changeTextview;
+    ArrayList<String> greetArray = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +24,11 @@ public class MainActivity extends AppCompatActivity {
         dog1 = findViewById(R.id.dog1);
         dog2 = findViewById(R.id.dog2);
         resourceTextview = findViewById(R.id.text);
-        changeTextview = findViewById(R.id.chagetext);
+        changeTextview = findViewById(R.id.changetext);
 
         // xml의 뷰 컴포넌트를 동적으로 변경하는 방법
         dog2.setImageResource(R.drawable.dog3);
-        resourceTextview.setText("여라나라 인사");
+        resourceTextview.setText("여러나라 인사");
 
         // res에 정의된 string을 가져오는 방법
         String content = getApplicationContext().getResources().getString(R.string.define_string);
@@ -36,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 for (String greeting : getApplicationContext().getResources().getStringArray(R.array.define_string_array)){
                     Toast.makeText(getApplicationContext(),greeting,Toast.LENGTH_SHORT).show();
+                    greetArray.add(greeting);
+                }
+                System.out.println(greetArray);
+                resourceTextview.append("\n");
+                for (String greet : greetArray){
+                    resourceTextview.append(greet+ ", ");
                 }
             }
         });

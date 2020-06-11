f5c2a81f42dee5b59f05046a6a8f314bc31135d8
newdecide <liberty-1@nate.com>
2020-06-11 21:33:38 +0900

    [추가] 안드로이드 기초_문자 입력 변경 과정 뷰에 보여주기

--- 03_Adnroid/07_TextChangeListener/app/src/main/java/com/study/myapplication/MainActivity.java
+++ 03_Adnroid/07_TextChangeListener/app/src/main/java/com/study/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView changetext = findViewById(R.id.changetext);
         inputText = findViewById(R.id.inputtext);
         inputText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -24,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("beforeTextChanged","start : " + start);
                 Log.d("beforeTextChanged","count : " + count);
                 Log.d("beforeTextChanged","after : " + after);
+
+                changetext.append("beforeTextChanged s : " + s + "\n");
+                changetext.append("beforeTextChanged start : " + start+ "\n");
+                changetext.append("beforeTextChanged count : " + count+ "\n");
+                changetext.append("beforeTextChanged after : " + after+ "\n");
+
             }
 
             @Override
@@ -32,11 +40,18 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("onTextChanged","start : " + start);
                 Log.d("onTextChanged","before : " + before);
                 Log.d("onTextChanged","count : " + count);
+
+                changetext.append("onTextChanged s : " + s+ "\n");
+                changetext.append("onTextChanged start : " + start+ "\n");
+                changetext.append("onTextChanged before : " + before+ "\n");
+                changetext.append("onTextChanged count : " + count+ "\n");
+
             }
 
             @Override
             public void afterTextChanged(Editable s) {
                 Log.d("afterTextChanged", "s : "+ s);
+                changetext.append("afterTextChanged s : "  + s+ "\n\n");
             }
         });
     }
--- 03_Adnroid/07_TextChangeListener/app/src/main/java/com/study/myapplication/README.md
+++ 03_Adnroid/07_TextChangeListener/app/src/main/java/com/study/myapplication/README.md
@@ -11,3 +11,5 @@
 <img src="https://user-images.githubusercontent.com/6762927/84253230-a6a54500-ab4a-11ea-929d-e46804b0a12d.png" width="60%">
 
 <img src="https://user-images.githubusercontent.com/6762927/84253301-ba50ab80-ab4a-11ea-940f-e12ad5c80410.png" width="60%">
+
+<img src="https://user-images.githubusercontent.com/6762927/84385563-c3b14500-ac2a-11ea-83da-118a1510288d.png" width ="40%">

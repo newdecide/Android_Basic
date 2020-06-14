736bced05bf6759e79bc881964dd045935106342
newdecide <liberty-1@nate.com>
2020-06-14 21:14:58 +0900

    [추가] 안드로이드 기초_코틀린 Any타입으로 여러 타입 화면에 출력

--- 04_Kotlin/Kotlin/app/src/main/java/com/study/myapplication/MainActivity.kt
+++ 04_Kotlin/Kotlin/app/src/main/java/com/study/myapplication/MainActivity.kt
@@ -19,6 +19,32 @@ class MainActivity : AppCompatActivity() {
         current_time_textview.setText(current_time)
 
 
+        var AnyType : Any
 
+        AnyType = "문자열 입력"
+        if (AnyType is String){
+            ViewWrite("String 타입 입니다.")
+        }
+        AnyType =12.00f
+        if (AnyType is Float){
+            ViewWrite("Float 타입 입니다.")
+        }
+        AnyType = 1234.5678
+        if (AnyType is Double){
+            ViewWrite("Double 타입 입니다.")
+        }
+        AnyType = 1234
+        if (AnyType is Int){
+            ViewWrite("Int 타입 입니다.")
+        }
+        AnyType = 123456789L
+        if (AnyType is Long){
+            ViewWrite("Long 타입 입니다.")
+        }
+
+    }
+    fun ViewWrite( any : Any){
+        var view_write = findViewById<TextView>(R.id.writetext)
+        view_write.text = "${view_write.text}${any.toString()}\n"
     }
 }
